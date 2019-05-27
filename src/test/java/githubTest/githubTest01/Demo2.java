9111165515777c3b825f7ee13596b9efe3751706
zhongcs123 <947426700@qq.com>
2019-05-27 10:28:53 +0800

    测试版本V3.0.0

---
+++ src/test/java/githubTest/githubTest01/Demo2.java
@@ -0,0 +1,24 @@
+package githubTest.githubTest01;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.AfterClass;
+
+public class Demo2 {
+  @Test
+  public void test() throws Exception {
+	  
+	  throw new Exception("测试2：错误信息");
+  }
+  @BeforeClass
+  public void beforeClass() {
+	  System.out.println("测试2");
+	  
+  }
+
+  @AfterClass
+  public void afterClass() {
+	  System.out.println("测试2");
+  }
+
+}
